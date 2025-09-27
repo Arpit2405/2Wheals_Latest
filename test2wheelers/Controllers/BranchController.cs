3b3b0bd4e974e7385a5f091b0cf19e00d3594c0e
Arpit Kumar <akakakarpit987106@gmail.com>
2025-09-27 13:15:32 +0530

    s

---
+++ test2wheelers/Controllers/BranchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2whealers.Controllers
+{
+    public class BranchController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
